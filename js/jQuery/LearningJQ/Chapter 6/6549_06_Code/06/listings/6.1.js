1fa90cc102b8919b363f649593b11f6c0afa8bb6
keighty <k80.leonard@gmail.com>
2013-08-06 13:06:03 -0700

    create gh-pages branch

--- js/jQuery/LearningJQ/Chapter 6/6549_06_Code/06/listings/6.1.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#letter-a a').click(function() {
-    $('#dictionary').load('a.html');
-    return false;
-  });
-});
--- js/jQuery/LearningJQ/Chapter 6/6549_06_Code/06/listings/6.2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#letter-a a').click(function() {
-    $('#dictionary').load('a.html');
-    alert('Loaded!');
-    return false;
-  });
-});
